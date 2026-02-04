2cd39b4103d548b940789756f6d46a20e713ad44
yair ben zur <benzuryair007@gmail.com>
2026-02-04 15:56:27 +0200

    הוספת פונקצית חילוק

--- calculator.py
+++ calculator.py
@@ -5,6 +5,14 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a,b):
+
+def multiply(a, b):
     """multiply 2 numbers"""
-    return a*b
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "It is not possible to divide by 0."
+    else:
+        return a / b
